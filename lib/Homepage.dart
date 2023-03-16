2ba217669f4a6c70c8be968d22f97711455e8298
Kaushal Dasadiya <kaushal.dasadiya.kd@gmail.com>
2023-03-16 18:58:48 +0530

    Update Homepage.dart

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -72,7 +72,7 @@ class Homepage extends StatelessWidget {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => home_screen()));
                 },
-                icon: Icon(Icons.logout_outlined))
+                icon: Icon(Icons.logout_outlined)),
           ],
         ),
         body: mystate(),
